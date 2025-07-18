30dc15cf4d89f88f265e682e8dd8dcd76e57f49d
Arthakker <arthuronyeanusi@gmail.com>
2025-07-18 04:39:57 +0100

    Added app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import authRoutes from "./src/routes/authRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use("/api/auth", authRoutes);
+// app.use('/api/clearance', clearanceRoutes);
+
+app.get("/", (req, res) =>
+  res.send("Online Clearance API built by Edoho Daniel is running ✅")
+);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
